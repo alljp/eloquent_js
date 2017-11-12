c9f136d9ccb9f8c62bee8bd70d7b364b7358f721
alljp <allenjosesmailbox@gmail.com>
2017-11-12 21:57:23 +0530

    Solution to Chapter6 problem 1 - A vector type

---
+++ Chapter6/Vector.js
@@ -0,0 +1,20 @@
+function Vector (x, y) {
+  this.x = x
+  this.y = y
+}
+
+Vector.prototype.plus = function (vector) {
+  return new Vector(this.x + vector.x, this.y + vector.y)
+}
+
+Vector.prototype.minus = function (vector) {
+  return new Vector(this.x - vector.x, this.y - vector.y)
+}
+
+Object.defineProperty(Vector.prototype, 'length', {
+  get: function () { return Math.pow((Math.pow(this.x, 2) + Math.pow(this.y, 2)), 0.5) }
+})
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)))
+console.log(new Vector(1, 2).minus(new Vector(2, 3)))
+console.log(new Vector(3, 4).length)
