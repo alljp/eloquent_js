c931010c20a33f0c752916ed13c213e30dbb2e0a
alljp <allenjosesmailbox@gmail.com>
2017-11-15 12:49:26 +0530

    Solution to Chapter11 problem 1 - Arrays

---
+++ Chapter11/Array.js
@@ -0,0 +1,27 @@
+let pl = require('./A_programming_language.js')
+
+let run = pl.run
+let topEnv = pl.topEnv
+
+topEnv['array'] = function () {
+  return Array.prototype.slice.call(arguments, 0)
+}
+
+topEnv['length'] = function (array) {
+  return array.length
+}
+
+topEnv['element'] = function (array, i) {
+  return array[i]
+}
+
+run('do(define(sum, fun(array,',
+    '     do(define(i, 0),',
+    '        define(sum, 0),',
+    '        while(<(i, length(array)),',
+    '          do(define(sum, +(sum, element(array, i))),',
+    '             define(i, +(i, 1)))),',
+    '        sum))),',
+    '   print(sum(array(1, 2, 3))))')
+
+run('print(array(2,4,1))')
