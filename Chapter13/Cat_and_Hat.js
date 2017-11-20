8d3dd527f825e945093e5e1acfb628f6c3b81f63
alljp <allenjosesmailbox@gmail.com>
2017-11-20 08:56:43 +0530

    Solution to Chapter13 problem 3 - The cat's hat

---
+++ Chapter13/Cat_and_Hat.js
@@ -0,0 +1,16 @@
+let cat = document.querySelector('#cat')
+let hat = document.querySelector('#hat')
+
+let angle = 0, lastTime = null
+function animate (time) {
+  if (lastTime != null) {
+    angle += (time - lastTime) * 0.0015
+  }
+  lastTime = time
+  cat.style.top = (Math.sin(angle) * 50 + 80) + 'px'
+  cat.style.left = (Math.cos(angle) * 200 + 230) + 'px'
+  let hatAngle = angle + Math.PI
+  hat.style.top = (Math.sin(hatAngle) * 50 + 80) + 'px'
+  hat.style.left = (Math.cos(hatAngle) * 200 + 230) + 'px'
+  requestAnimationFrame(animate)
+}
