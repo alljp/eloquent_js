d16c5cc4855f7b50d68a6a12ce98251bae9f9d6f
alljp <allenjosesmailbox@gmail.com>
2017-11-21 12:41:43 +0530

    Add definition for Lava constructor

--- Chapter15/game.js
+++ Chapter15/game.js
@@ -60,4 +60,17 @@ function Coin (pos) {
   this.wobble = Math.random() * Math.PI * 2
 }
 Coin.prototype.type = 'coin'
- 
+
+function Lava (pos, ch) {
+  this.pos = pos
+  this.size = new Vector(1, 1)
+  if (ch === '=') {
+    this.speed = new Vector(2, 0)
+  } else if (ch === '|') {
+    this.speed = new Vector(0, 2)
+  } else if (ch === 'v') {
+    this.speed = new Vector(0, 3)
+    this.repeatPos = pos
+  }
+}
+Lava.prototype.type = 'lava'
