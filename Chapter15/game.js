9ea414f77e490db4006539851c5173e518d4d227
alljp <allenjosesmailbox@gmail.com>
2017-11-21 12:29:38 +0530

    Add definition for Player constructor

--- Chapter15/game.js
+++ Chapter15/game.js
@@ -46,3 +46,10 @@ let actorChars = {
   '|': Lava,
   'v': Lava
 }
+
+function Player (pos) {
+  this.pos = pos.plus(new Vector(0, -0.5))
+  this.size = new Vector(0.8, 1.5)
+  this.speed = new Vector(0, 0)
+}
+Player.prototype.type = 'player'
