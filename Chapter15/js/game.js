6c0982f7642f6932f1a4e6834a7054213a1b9dcd
alljp <allenjosesmailbox@gmail.com>
2017-11-22 16:51:48 +0530

    Add definition for function trackKeys
    Returns an object that tracks the current position of the keys

--- Chapter15/js/game.js
+++ Chapter15/js/game.js
@@ -307,3 +307,17 @@ Level.prototype.playerTouched = function (type, actor) {
     this.finishDelay = 1
   }
 }
+
+function trackKeys (codes) {
+  let pressed = Object.create(null)
+  function handler (event) {
+    if (codes.hasOwnProperty(event.keyCode)) {
+      let down = event.type === 'keydown'
+      pressed[codes[event.keyCode]] = down
+      event.preventDefault()
+    }
+  }
+  addEventListener('keydown', handler)
+  addEventListener('keyup', handler)
+  return pressed
+}
